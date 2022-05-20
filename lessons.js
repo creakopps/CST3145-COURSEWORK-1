2d9e08c93beb9ce5fa685e0fef1a259f769d0f61
creakopps <77118099+creakopps@users.noreply.github.com>
2022-05-20 18:07:01 +0100

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,83 @@
+
+   let subject = [
+        {
+            "id": 1,
+            "lesson": "Math",
+            "Location": "London",
+            "Price": 120,
+            "Space": 5,
+            "image": "Maths.png"
+        },
+        {
+            "id": 2,
+            "lesson": "English",
+            "Location": "Enfield",
+            "Price": 90,
+            "Space": 5,
+            "image": "English.png"
+        },
+        {
+            "id": 3,
+            "lesson": "Science",
+            "Location": "Hackney",
+            "Price": 100,
+            "Space": 5,
+            "image": "Science.png"
+        },
+        {
+            "id": 4,
+            "lesson": "English",
+            "Location": "York",
+            "Price": 80,
+            "Space": 5,
+            "image": "English.png"
+        },
+        {
+            "id": 5,
+            "lesson": "ICT",
+            "Location": "Manchester",
+            "Price": 80,
+            "Space": 5,
+            "image": "ICT.png"
+        },
+        {
+            "id": 6,
+            "lesson": "Math",
+            "Location": "Chelsea",
+            "Price": 120,
+            "Space": 5,
+            "image": "Math.png"
+        },
+        {
+            "id": 7,
+            "lesson": "English",
+            "Location": "Liverpool",
+            "Price": 120,
+            "Space": 5,
+            "image": "English.png"
+        },
+        {
+            "id": 8,
+            "lesson": "Science",
+            "Location": "Leeds",
+            "Price": 70,
+            "Space": 5,
+            "image": "Science.png"
+        },
+        {
+            "id": 9,
+            "lesson": "Art",
+            "Location": "Swansea ",
+            "Price": 150,
+            "Space": 5,
+            "image": "Art.png"
+        },
+        {
+            "id": 10,
+            "lesson": "Music",
+            "Location": "Tottenham",
+            "Price": 170,
+            "Space": 5,
+            "image": "Music.png"
+        }
+    ]
